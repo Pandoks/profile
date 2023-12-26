7103330a0abba6fc27161780baa22ef670a2eba3
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-12-26 18:04:16 -0500

    Different plaid clients depending on environment

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -16,17 +16,29 @@ const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
 const PLAID_ENV = process.env.PLAID_ENV || "sandbox";
 
-const configuration = new Configuration({
-  basePath: PlaidEnvironments[PLAID_ENV],
-  baseOptions: {
-    headers: {
-      "PLAID-CLIENT-ID": PLAID_CLIENT_ID,
-      "PLAID-SECRET": PLAID_SECRET,
-      "Plaid-Version": "2020-09-14",
+let plaid: any;
+if (PLAID_ENV === "sandbox") {
+  const plaid_configuration = new Configuration({
+    basePath: PlaidEnvironments[PLAID_ENV],
+    baseOptions: {
+      headers: {
+        "PLAID-CLIENT-ID": PLAID_CLIENT_ID,
+        "PLAID-SECRET": PLAID_SECRET,
+        "Plaid-Version": "2020-09-14",
+      },
     },
-  },
-});
-const plaid = new PlaidApi(configuration);
+  });
+  plaid = new PlaidApi(plaid_configuration);
+} else if (PLAID_ENV === "production") {
+  const plaid_sdk = require("plaid");
+  plaid = new plaid_sdk.Client({
+    clientID: [PLAID_CLIENT_ID],
+    secret: [PLAID_SECRET],
+    env: PlaidEnvironments.production,
+  });
+} else {
+  throw new Error("Invalid Plaid environment");
+}
 /**
  * Plaid FLOW
  *
