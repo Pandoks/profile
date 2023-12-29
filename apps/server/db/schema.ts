6e2bf140512c5fe0db797293c60aab72971cc65e
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-12-29 18:24:47 -0500

    Defined auth db schema

--- apps/server/db/schema.ts
+++ apps/server/db/schema.ts
@@ -1,5 +1,19 @@
-import { mysqlTable, serial } from "drizzle-orm/mysql-core";
+import { datetime, mysqlTable, varchar } from "drizzle-orm/mysql-core";
 
 export const users = mysqlTable("users", {
-  id: serial("id").primaryKey(),
+  id: varchar("id", {
+    length: 255,
+  }).primaryKey(),
+});
+
+export const sessions = mysqlTable("sessions", {
+  id: varchar("id", {
+    length: 255,
+  }).primaryKey(),
+  userId: varchar("user_id", {
+    length: 255,
+  })
+    .notNull()
+    .references(() => users.id),
+  expiration: datetime("expiration").notNull(),
 });
