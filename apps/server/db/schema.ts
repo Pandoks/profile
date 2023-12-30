55f10652266b0bc7f8442bab53d954a3e630fa0e
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-12-29 21:01:56 -0500

    Added username and password to users table

--- apps/server/db/schema.ts
+++ apps/server/db/schema.ts
@@ -1,9 +1,11 @@
-import { datetime, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import { datetime, mysqlTable, text, varchar } from "drizzle-orm/mysql-core";
 
 export const users = mysqlTable("users", {
   id: varchar("id", {
     length: 255,
   }).primaryKey(),
+  username: text("username").notNull().unique(),
+  hashedPassword: text("hashed_password").notNull(),
 });
 
 export const sessions = mysqlTable("sessions", {
